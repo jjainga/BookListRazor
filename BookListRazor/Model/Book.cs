530c4b988a09dd81777f5a29ded9babce9cc7c71
Joshua Jainga <jjainga@gmail.com>
2021-06-14 18:33:39 -0700

    Added create and edit pages

--- BookListRazor/Model/Book.cs
+++ BookListRazor/Model/Book.cs
@@ -14,7 +14,6 @@ namespace BookListRazor.Model
         [Required]
         public string Title { get; set; }
         public string Author { get; set; }
-
-
+        public string ISBN { get; set; }
     }
 }
