0255924601f9013b4100a5197673eda33f94fad0
Joshua Jainga <jjainga@gmail.com>
2021-06-16 13:21:52 -0700

    added route for a api to get book json data

---
+++ BookListRazor/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookListRazor.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            return JSON(new {data = _db.Book.ToList()});
+        }
+    }
+}
