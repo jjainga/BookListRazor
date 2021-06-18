7c9494d1edddbe95f48eca6c99192a6b2d0161e0
Joshua Jainga <jjainga@gmail.com>
2021-06-18 10:01:04 -0700

    delete request

--- BookListRazor/wwwroot/js/bookList.js
+++ BookListRazor/wwwroot/js/bookList.js
@@ -12,9 +12,9 @@ function loadDataTable() {
             "datatype": "json"
         },
         "columns": [
-            {"data": "title", "width":"30%"},
-            {"data": "author", "width":"30%"},
-            {"data": "isbn", "width":"30%"},
+            {"data": "title", "width":"20%"},
+            {"data": "author", "width":"20%"},
+            {"data": "isbn", "width":"20%"},
             {
                 "data": "id",
                 "render": function(data) {
@@ -22,10 +22,15 @@ function loadDataTable() {
                             <a href="/Books/Edit?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
                                 Edit
                             </a>
+                            &nbsp;
+                            <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;' 
+                            onclick=Delete('/api/book?id='+${data})>
+                                Delete
+                            </a>
                             
                             
                         </div>`;
-                    }, "width": "30%"
+                    }, "width": "40%"
             }
         ],
         "language": {
@@ -34,3 +39,31 @@ function loadDataTable() {
         "width": "100%"
     });
 }
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if(willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if(data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+
+                }
+            });
+        }
+    });
+}
+
